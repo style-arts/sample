c00312cee55a5a3b2b412aa228ada078a0716865
style-arts <104714748+style-arts@users.noreply.github.com>
2022-05-01 19:50:02 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1657100236-mYaWDR21;
+    var liffId = "自分のLIFFIDを入力";
     initializeLiff(liffId);
 })
 
@@ -51,4 +51,3 @@ function shareTargetPicker(text) {
         window.alert('Failed to send message ' + error);
     });
 }
-
