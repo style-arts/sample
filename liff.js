f085c235286d8e79eddfff98324b91d3ea795b1e
style-arts <104714748+style-arts@users.noreply.github.com>
2022-05-01 20:17:12 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "自分のLIFFIDを入力";
+    var liffId = "1657100236-mYaWDR21";
     initializeLiff(liffId);
 })
 
